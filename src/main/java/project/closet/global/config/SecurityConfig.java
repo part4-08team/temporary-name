6010feb8d8946ebd2d5df2e7858ea7782df7e1f3
이요한 <icb1696@naver.com>
2025-06-25 09:50:04 +0900

    초기 세팅

---
+++ src/main/java/project/closet/global/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package project.closet.global.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
---
+++ src/test/java/project/closet/ClosetApplicationTests.java
@@ -0,0 +1,13 @@
+package project.closet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetApplicationTests {
+
+//  @Test
+//  void contextLoads() {
+//  }
+
+}
