94efa9b355fa9089c8b8208966cd991ced1edcc9
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-07-02 16:25:40 +0900

    feat: CSRF 토큰 발행 API 엔드포인트 매핑

--- src/main/java/project/closet/auth/controller/AuthController.java
+++ src/main/java/project/closet/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.closet.auth.controller.api.AuthApi;
@@ -16,6 +17,7 @@ import project.closet.auth.controller.api.AuthApi;
 public class AuthController implements AuthApi {
 
     @Override
+    @GetMapping("/csrf-token")
     public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
         log.debug("CSRF 토큰 요청");
         return ResponseEntity.status(HttpStatus.OK).body(csrfToken);
--- src/main/java/project/closet/global/config/security/SecurityMatchers.java
+++ src/main/java/project/closet/global/config/security/SecurityMatchers.java
@@ -10,11 +10,11 @@ public abstract class SecurityMatchers {
     public static final RequestMatcher NON_API =
             new NegatedRequestMatcher(new AntPathRequestMatcher("/api/**"));
 
-  public static final RequestMatcher GET_CSRF_TOKEN =
-          new AntPathRequestMatcher("/api/auth/csrf-token", HttpMethod.GET.name());
+    public static final RequestMatcher GET_CSRF_TOKEN =
+            new AntPathRequestMatcher("/api/auth/csrf-token", HttpMethod.GET.name());
 
     public static final RequestMatcher SIGN_UP =
-            new AntPathRequestMatcher("/api/auth/sign-in");
+            new AntPathRequestMatcher("/api/users", HttpMethod.PATCH.name());
 
     public static final RequestMatcher[] PUBLIC_MATCHERS = new RequestMatcher[]{
             NON_API, GET_CSRF_TOKEN, SIGN_UP
