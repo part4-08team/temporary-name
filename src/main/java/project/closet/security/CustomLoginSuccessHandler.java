33ab49cf20bb8864164c3c61f2a3fe5aefe10e1e
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-07-02 18:31:51 +0900

    feat: 로그인을 담당하는 JsonUsernameFilter 로직 성공과 실패 핸들러 등록

---
+++ src/main/java/project/closet/security/CustomLoginSuccessHandler.java
@@ -0,0 +1,27 @@
+package project.closet.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@RequiredArgsConstructor
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+        ClosetUserDetails principal = (ClosetUserDetails) authentication.getPrincipal();
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(principal.getUserDto()));
+    }
+}
