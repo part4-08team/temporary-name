87baf374e40d0f18a7a958165e7cbf639ab1cdc1
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-07-03 10:22:55 +0900

    Fix: 로그인 시 email로 받을 수 있도록 변경

--- src/main/java/project/closet/dto/request/LoginRequest.java
+++ src/main/java/project/closet/dto/request/LoginRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
         @NotBlank(message = "사용자 이름은 필수입니다")
-        String username,
+        String email,
 
         @NotBlank(message = "비밀번호는 필수입니다")
         String password
