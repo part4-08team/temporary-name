f7d46087f63fe822655c1dfb8650786b0fde56e8
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-07-04 10:28:30 +0900

    Refactor: Auth API 설정

--- src/main/java/project/closet/weather/controller/WeatherController.java
+++ src/main/java/project/closet/weather/controller/WeatherController.java
@@ -1,9 +1,13 @@
 package project.closet.weather.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.closet.dto.response.WeatherAPILocation;
 import project.closet.weather.controller.api.WeatherApi;
 
 @Slf4j
@@ -12,4 +16,21 @@ import project.closet.weather.controller.api.WeatherApi;
 @RequestMapping("/api/weathers")
 public class WeatherController implements WeatherApi {
 
+    @GetMapping
+    @Override
+    public ResponseEntity<List<String>> getWeatherInfo(
+            Double longitude,
+            Double latitude
+    ) {
+        return null;
+    }
+
+    @GetMapping("/location")
+    @Override
+    public ResponseEntity<WeatherAPILocation> getWeatherLocation(
+            Double longitude,
+            Double latitude
+    ) {
+        return null;
+    }
 }
--- src/main/java/project/closet/weather/controller/api/WeatherApi.java
+++ src/main/java/project/closet/weather/controller/api/WeatherApi.java
@@ -1,8 +1,47 @@
 package project.closet.weather.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import project.closet.dto.response.WeatherAPILocation;
 
 @Tag(name = "날씨 관리", description = "날씨 관련 API")
 public interface WeatherApi {
 
+    @Operation(summary = "날씨 정보 조회", description = "날씨 정보 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "날씨 정보 조회 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "400", description = "날씨 정보 조회 실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
+    ResponseEntity<List<String>> getWeatherInfo(
+            @Parameter Double longitude,
+            @Parameter Double latitude
+    );
+
+    @Operation(summary = "날씨 위치 정보 조회", description = "날씨 위치 정보 조회 API")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "날씨 위치 정보 조회 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "400", description = "날씨 위치 정보 조회 실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
+    ResponseEntity<WeatherAPILocation> getWeatherLocation(
+            @Parameter Double longitude,
+            @Parameter Double latitude
+    );
 }
