3607fd348953121a8ba03b403896bb259de2a66c
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-07-08 10:40:54 +0900

    Feat: 날씨 API 비동기 설정 파일 생성

---
+++ src/main/java/project/closet/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package project.closet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    // OpenAPI 호출 비동기 설정
+    @Bean(name = "weatherExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("weather-api-async-");
+        executor.initialize();
+        return executor;
+    }
+}
