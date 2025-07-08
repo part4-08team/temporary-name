4fb22cb64465d03d9051d0e18e3359d89a48b9f3
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-07-08 14:25:24 +0900

    Refactor: 사용하지 않는 클래스 파일 삭제

--- src/main/java/project/closet/domain/clothes/dto/response/ClothesAttributeDto.java
+++
@@ -1,8 +0,0 @@
-package project.closet.domain.clothes.dto.response;
-
-import java.util.UUID;
-
-public record ClothesAttributeDto(
-        UUID definitionId,   // 속성 정의 ID
-        String value         // 속성 값
-) {}
