7cc2589b846a2165a9329ed8e43d9b31b6c2dc4e
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-07-20 16:20:58 +0900

    Feat: 비밀번호 초기화 로직 구현

--- src/main/java/project/closet/mail/MailService.java
+++ src/main/java/project/closet/mail/MailService.java
@@ -1,15 +1,22 @@
 package project.closet.mail;
 
+import jakarta.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class MailService {
+
+    public static final String STATIC_PASSWORD_RESET_HTML = "static/password-reset.html";
 
     private final JavaMailSender mailSender;
 
@@ -25,4 +32,28 @@ public class MailService {
         log.info("Sending email to: {}, subject: {}", to, subject);
         mailSender.send(message);
     }
+
+    public void sendMimeMail(
+            String to,
+            String subject,
+            String text
+    ) {
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+
+            ClassPathResource resource = new ClassPathResource(STATIC_PASSWORD_RESET_HTML);
+            String html = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+            html = html.replace("${tempPassword}", text);
+
+            mimeMessageHelper.setText(html, true);
+            mailSender.send(message);
+        } catch (Exception e) {
+            log.error("Error while sending MIME email", e);
+            throw new RuntimeException("Failed to send MIME email", e);
+        }
+    }
+
 }
